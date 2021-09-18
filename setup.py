ae676f5208ed18de38db290d4602d2f08c3361d4
Sandeep Kumar Rana <rana.sandeep3119@gmail.com>
2021-09-18 19:02:11 +0530

    Added tox

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wineq package",
+    author="Sandeep",
+    packages=find_packages(),
+    license="MIT"
+
+)
